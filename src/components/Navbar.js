6582cd3ae9439733d32c3371c09f9addb3fe8163
MomchilTodorov <52608910+MomchilTodorov@users.noreply.github.com>
2019-09-29 14:19:10 +0100

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,26 +7,34 @@ import GoogleAuth from './googleauth/GoogleAuth';
 class Navbar extends Component {
   render() {
     return (
-      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-        <Link to="/">
-          <i className="fas fa-laptop-code fa-2x" />
-        </Link>
-        <ul className="navbar-nav align-items-center">
-          <li className="nav-item ml-5">
+      <NavWrapper className="navbar">
+        <ul className="ul-nav">
+          <li className="li-nav">
+            <Link to="/">
+              <i className="fas fa-laptop-code fa-2x" />
+            </Link>
+          </li>
+          <li className="li-nav">
             <Link to="/" className="nav-link">
               <p id="products">Products</p>
             </Link>
           </li>
         </ul>
-        <GoogleAuth />
-        <Link to="/cart" className="ml-auto">
-          <ButtonContainer>
-            <span className="mr-2">
-              <i className="fas fa-cart-plus" />
-            </span>
-            My cart
-          </ButtonContainer>
-        </Link>
+        <ul className="ul-nav cart-ul">
+          <li className="li-nav">
+            <Link to="/cart">
+              <ButtonContainer className="cart">
+                <span>
+                  <i className="fas fa-cart-plus" />
+                </span>
+                My cart
+              </ButtonContainer>
+            </Link>
+          </li>
+        </ul>
+        <div className="b-wrap">
+          <GoogleAuth className="googleButton" />
+        </div>
       </NavWrapper>
     );
   }

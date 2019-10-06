eed12d9eb477614223ce7b8806fe258b99efc729
MomchilTodorov <52608910+MomchilTodorov@users.noreply.github.com>
2019-10-06 18:02:51 +0100

    Add files via upload

--- src/components/cart/CartTotals.js
+++ src/components/cart/CartTotals.js
@@ -1,44 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PayPalButton from './PayPalButton';
+import { connect } from 'react-redux';
+import { clearCart } from '../../actions';
 
-function CartTotals({ value, history }) {
-  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
-  return (
-    <React.Fragment>
-      <div className="container"></div>
-      <div className="row">
-        <div className="col-10 mt-2 mr-lg-5 ml-sm-auto mr-sm-4 ml-md-auto mr-md-4 col-sm-8 text-capitalize text-right">
-          <Link to="/">
-            <button
-              className="btn btn-outline-danger text-uppercase mb-3 px-5"
-              type="button"
-              onClick={() => clearCart()}
-            >
-              clear cart
-            </button>
-          </Link>
-          <h5>
-            <span className="text-title">subtotal :</span>
-            <strong>${cartSubTotal}</strong>
-          </h5>
-          <h5>
-            <span className="text-title">tax :</span>
-            <strong>${cartTax}</strong>
-          </h5>
-          <h5>
-            <span className="text-title">total :</span>
-            <strong>${cartTotal}</strong>
-          </h5>
-          <PayPalButton
-            total={cartTotal}
-            clearCart={clearCart}
-            history={history}
-          />
+class CartTotals extends Component {
+  render() {
+    const { cartSubTotal, cartTax, cartTotal } = this.props.data;
+    return (
+      <React.Fragment>
+        <div className="container"></div>
+        <div className="row">
+          <div className="col-10 mt-2 mr-lg-5 ml-sm-auto mr-sm-4 ml-md-auto mr-md-4 col-sm-8 text-capitalize text-right">
+            <Link to="/">
+              <button
+                className="btn btn-outline-danger text-uppercase mb-3 px-5"
+                type="button"
+                onClick={() => {
+                  this.props.clearCart();
+                }}
+              >
+                clear cart
+              </button>
+            </Link>
+            <h5>
+              <span className="text-title">subtotal :</span>
+              <strong>${cartSubTotal}</strong>
+            </h5>
+            <h5>
+              <span className="text-title">tax :</span>
+              <strong>${cartTax}</strong>
+            </h5>
+            <h5>
+              <span className="text-title">total :</span>
+              <strong>${cartTotal}</strong>
+            </h5>
+            <PayPalButton total={cartTotal} clearCart={this.props.clearCart} />
+          </div>
         </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
-export default CartTotals;
+function mapStateToProps(state) {
+  return { data: state.data };
+}
+
+export default connect(
+  mapStateToProps,
+  { clearCart }
+)(CartTotals);
--- src/components/cart/PayPalButton.js
+++ src/components/cart/PayPalButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PaypalExpressBtn from 'react-paypal-express-checkout';
+import history from '../../history';
 
 export default class MyApp extends React.Component {
   render() {
@@ -7,7 +8,7 @@ export default class MyApp extends React.Component {
       // Congratulation, it came here means everything's fine!
       console.log('The payment was succeeded!', payment);
       this.props.clearCart();
-      this.props.history.push('/');
+      history.push('/');
       // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
     };
 
@@ -30,7 +31,8 @@ export default class MyApp extends React.Component {
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: 'Ab3cYbvVYB3Dkri7racmpnRND9GN9jy9W5Ei1enarYZyf6r77NTVTf8WIPzPng26UF6mqvoLq66scF2Z',
+      sandbox:
+        'Ab3cYbvVYB3Dkri7racmpnRND9GN9jy9W5Ei1enarYZyf6r77NTVTf8WIPzPng26UF6mqvoLq66scF2Z',
       production: 'YOUR-PRODUCTION-APP-ID'
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first

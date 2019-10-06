54dfc702f418cf4493adc5ddf288e9009d46d3a2
MomchilTodorov <52608910+MomchilTodorov@users.noreply.github.com>
2019-10-06 18:38:02 +0100

    Add files via upload

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,30 +2,34 @@ import React, { Component } from 'react';
 import Title from '../Title';
 import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
+import { ProductConsumer } from '../../context';
 import CartList from './CartList';
 import CartTotals from './CartTotals';
-import { connect } from 'react-redux';
 
 class Cart extends Component {
   render() {
-    const { cart } = this.props.data;
-    if (cart.length > 0) {
-      return (
-        <React.Fragment>
-          <Title name="your" title="cart" />
-          <CartColumns />
-          <CartList />
-          <CartTotals />
-        </React.Fragment>
-      );
-    } else {
-      return <EmptyCart />;
-    }
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColumns />
+                  <CartList value={value} />
+                  <CartTotals value={value} history={this.props.history} />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    );
   }
 }
 
-function mapStateToProps(state) {
-  return { data: state.data };
-}
-
-export default connect(mapStateToProps)(Cart);
+export default Cart;
